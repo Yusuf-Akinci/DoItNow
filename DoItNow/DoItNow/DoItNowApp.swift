7eb4486f220ad1f0b72589142ea4a3547c4e0699
Yusuf-Akinci <yusufk.akinci@gmail.com>
2026-06-21 22:42:21 +0200

    Add project to repo.

---
+++ DoItNow/DoItNow/DoItNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoItNowApp.swift
+//  DoItNow
+//
+//  Created by Yusuf Akinci on 21.06.26.
+//
+
+import SwiftUI
+
+@main
+struct DoItNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
