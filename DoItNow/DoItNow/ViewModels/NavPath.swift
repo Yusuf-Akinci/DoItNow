7b70cad54ef90f692fa677648f95abac2210602e
Yusuf-Akinci <yusufk.akinci@gmail.com>
2026-06-24 17:53:00 +0200

    navigation logic

---
+++ DoItNow/DoItNow/ViewModels/NavPath.swift
@@ -0,0 +1,12 @@
+//
+//  NavPath.swift
+//  DoItNow
+//
+//  Created by Yusuf Akinci on 24.06.26.
+//
+import SwiftUI
+
+enum NavPath: Hashable{
+    case newItem
+    case details
+}
--- DoItNow/DoItNow/Views/HomeView.swift
+++ DoItNow/DoItNow/Views/HomeView.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 
 struct HomeView: View {
-    @State private var path = NavigationPath()
+    @State private var path: [NavPath] = [] //Alle navigations zustände werden schritt für schritt gespeichert
     @State private var toDoItems: [Item] = [Item(id: "abc23", authorId: "Yusuf", title: "fjdaslk", description: "jkfdl sdjfkl", startDate: .now, status: .todo, taskRelevance: .medium), Item(id: "fdas41", authorId: "Ahmet", title: "f32", description: "fjei vgg", startDate: .now, status: .todo, taskRelevance: .low)]
     @State private var inProgressItems: [Item] = [Item(id: "abc23", authorId: "Yusuf", title: "fjdaslk", description: "jkfdl sdjfkl", startDate: .now+4, status: .todo, taskRelevance: .medium), Item(id: "fdas41", authorId: "Ahmet", title: "f32", description: "fjei vgg", startDate: .now, status: .todo, taskRelevance: .medium)]
     @State private var doneItems: [Item] = [Item(id: "abc23", authorId: "Yusuf", title: "fjdaslk", description: "jkfdl sdjfkl", startDate: .now+6, status: .todo, taskRelevance: .medium), Item(id: "fdas41", authorId: "Ahmet", title: "f32", description: "fjei vgg", startDate: .now, status: .todo, taskRelevance: .medium)]
 
     var body: some View {
-        NavigationStack{
+        NavigationStack(path: $path){ //aendert sich der path, so wird der untere navDestination aufgerufen
             TabView{
                 ListView(title: "To Do", items: $toDoItems).tabItem({Image(systemName: "eye")})
                 ListView(title: "In Progress", items: $inProgressItems).tabItem({Image(systemName: "nose")})
@@ -29,13 +29,20 @@ struct HomeView: View {
                 }
                 ToolbarItem(placement: .topBarTrailing){
                     Button{
-                        print("Navigate to new Item")
+                        path.append(.newItem)
                     } label:{Image(systemName: "plus")}
                 }
             }
-        }
-        .navigationDestination(for: String.self){ _ in
-            Text("New view")
+            .navigationDestination(for: NavPath.self){ path in
+                switch path{
+                case .newItem:
+                    Text("New Item view!")
+                
+                case .details:
+                    Text("Item details view")
+                
+                }
+            }
         }
         
     }
